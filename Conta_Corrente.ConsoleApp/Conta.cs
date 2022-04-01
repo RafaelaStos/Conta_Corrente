55e5eec009f391c70f58c39df711c17eb9c5454d
Rafaela dos Santos Morais <morais.rafa508@gmail.com>
2022-04-01 14:17:47 -0300

    Add project files.

---
+++ Conta_Corrente.ConsoleApp/Conta.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta_Corrente.ConsoleApp
+{
+    public class Conta
+    {
+        public int numeroConta;
+        public double saldo;
+        public bool tipoConta;
+        public double limite;
+        public Movimentacao[] movimentacoesRealizadas;
+
+
+        public double Saldo()
+        {
+            return saldo;
+
+        }
+
+        public void Sacar(double quantidade)
+        { 
+            
+            if (quantidade < saldo)
+            {
+            Movimentacao novamovimentacao = new Movimentacao();
+
+            int idConta = registroMovimentação();
+            novamovimentacao.valor = quantidade;
+            novamovimentacao.tipoMovimentacao = "Debito";
+
+           
+                double novoSaldo = this.saldo - quantidade;
+                this.saldo = novoSaldo;
+
+               ApresentarMensagem("Saque realizada com sucesso!\n",ConsoleColor.Green);
+             movimentacoesRealizadas[idConta] = novamovimentacao;
+            }
+            else ApresentarMensagem("Não foi possivel realizar o saque. Saldo insuficiente!\n", ConsoleColor.Red);
+           
+           
+        }
+
+        public void Depositar(double quantidade)
+        {
+            int idConta = registroMovimentação();
+            Movimentacao novamovimentacao = new Movimentacao();
+            novamovimentacao.valor  = quantidade;
+            novamovimentacao.tipoMovimentacao = "Credito";
+
+            this.saldo += quantidade;
+
+            movimentacoesRealizadas[idConta] = novamovimentacao;
+            ApresentarMensagem("Deposito realizado com sucesso!",ConsoleColor.Green);
+        }
+
+        public void transferir(Conta destino, double valor)
+        {
+            if (valor < saldo)
+            {
+                int idConta = registroMovimentação();
+            Movimentacao novamovimentacao = new Movimentacao();
+            novamovimentacao.valor = valor;
+            novamovimentacao.tipoMovimentacao = "Tranferencia";
+                this.saldo -= valor;
+                destino.saldo += valor;
+                
+                movimentacoesRealizadas[idConta] = novamovimentacao;
+                ApresentarMensagem("Tranferencia Realizada com Sucesso!", ConsoleColor.Green);
+            }
+            
+            else ApresentarMensagem("Não foi possivel realizar a Tranferencia. Saldo insuficiente!", ConsoleColor.Red);
+
+            
+        }
+
+
+        public void Extrato()
+        {
+            string Conta;
+            if (this.tipoConta == true)
+                Conta = "Especial";
+
+            else
+           Conta = "Normal";
+
+            Console.WriteLine("=============================== EXTRATO ===============================");
+            Console.WriteLine("\nN° Conta: "+this.numeroConta+"\nSaldo da conta: "+this.saldo+
+                "\nTipo de conta:"+Conta+"\nLimite da Conta: "+this.limite);
+
+            Console.WriteLine("\n===================== MOVIMENTAÇÕES DA CONTA =====================\n");
+           
+            for (int i = 0; i < movimentacoesRealizadas.Length; i++)
+            {
+                if (movimentacoesRealizadas[i]!=null)
+                {
+                   if(movimentacoesRealizadas[i].tipoMovimentacao == "Debito")
+                    {
+                        ApresentarMensagem("\nSaque realizado da conta " + numeroConta +" com o valor de "+ movimentacoesRealizadas[i].valor,ConsoleColor.Red);
+                    }
+                   else if (movimentacoesRealizadas[i].tipoMovimentacao == "Credito")
+                    {
+                        ApresentarMensagem("\nDeposito efetuado na conta " + numeroConta + " com o valor de " + movimentacoesRealizadas[i].valor, ConsoleColor.Green);
+
+                    }
+                    else if (movimentacoesRealizadas[i].tipoMovimentacao == "Tranferencia")
+                    {
+
+                        ApresentarMensagem("\nTransferencia efetuada da conta " + numeroConta + " com o valor de " + movimentacoesRealizadas[i].valor, ConsoleColor.Red);
+
+
+                    }
+                }
+
+            }
+            Console.WriteLine("\n=====================================================================\n");
+        }
+        public int registroMovimentação()
+        {
+            int idConta=-1;
+            for (int i = 0; i < movimentacoesRealizadas.Length; i++)
+            {
+                if (movimentacoesRealizadas[i] == null)
+                    idConta = i;
+
+            }
+            return idConta;
+        }
+        public void EmitirSaldo()
+        {
+            Console.WriteLine("\n===================== SALDO =====================");
+            Console.WriteLine("\nN° Conta: "+this.numeroConta+"\nSaldo Atual: "+saldo);
+            Console.WriteLine("\n===============================================\n");
+        }
+        public void ApresentarMensagem(string mensagem, ConsoleColor cor)
+        {
+            Console.ForegroundColor = cor;
+
+            Console.WriteLine(mensagem);
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ Conta_Corrente.ConsoleApp/TesteContaCorrente.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace Conta_Corrente.ConsoleApp
+{
+    internal class TesteContaCorrente
+    {
+        static void Main(string[] args)
+        {
+            Conta conta1 = new Conta();
+
+            conta1.numeroConta = 12;
+            conta1.saldo = 1000;
+            conta1.limite = 0;
+            conta1.tipoConta = false;
+            conta1.movimentacoesRealizadas = new Movimentacao[10];
+
+            conta1.Sacar(200);
+            conta1.Depositar(300);
+            conta1.Depositar(500);
+            conta1.Sacar(200);
+
+           Conta  conta2 = new Conta();
+          conta2.numeroConta = 1235;
+          conta2.saldo = 1000;
+          conta2.tipoConta = true;
+          conta2.limite = 300;
+          conta2.movimentacoesRealizadas = new Movimentacao[10];
+
+
+             //realizando operações com a conta 1
+          conta1.Extrato();
+
+          conta1.EmitirSaldo();
+
+          conta1.Depositar(15);
+
+          conta1.EmitirSaldo();
+
+          //Saque inválido
+          conta1.Sacar(18000);
+
+          //Saque válido
+          conta1.Sacar(15);
+
+          conta1.Extrato();
+
+          //realizando operações com a conta 2
+          conta2.Extrato();
+
+          conta2.EmitirSaldo();
+
+          conta2.Depositar(15);
+
+          conta2.EmitirSaldo();
+
+          //Saque inválido
+          conta2.Sacar(18000);
+
+          //Saque válido
+          conta2.Sacar(15);
+
+          conta2.Extrato();
+
+            //realizando operações transfer~encias entre as contas
+            Console.WriteLine("\n=============================================================================================\n");
+
+
+         //valor excedido
+         conta1.transferir(conta2, 15000);
+        
+         //valor válido
+        conta1.transferir(conta2, 15);
+        
+         //Vendo a transferência na perspectiva da conta1
+         conta1.Extrato();
+        
+         //Vendo a transferência na perspectiva da conta2
+         conta2.Extrato();
+        
+         //valor excedido
+         conta2.transferir(conta1, 115000);
+        
+         //valor válido
+         conta2.transferir(conta1,15);
+        
+         //Vendo a transferência na perspectiva da conta2
+         conta2.Extrato();
+         conta2.EmitirSaldo();
+         //Vendo a transferência na perspectiva da conta1
+         conta1.Extrato();
+         conta1.EmitirSaldo();
+
+        }
+    }
+}
